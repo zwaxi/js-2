e9ec769c9ecec99a124f18b7267d12a65d8a4f63
zwaxi <muhammadishkuvatov@gmail.com>
2024-04-20 13:05:54 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+confirm("string qiymatini kiriting . . .");
+let string1 = prompt("string qiymatini kiriting . . .");
+confirm("boolean qiymatini kiriting . . .");
+let boolean1 = Boolean(prompt("boolean qiymatini kiriting . . ."));
+confirm("number qiymatini kiriting . . .");
+let number1 = Number(prompt("number qiymatini kiriting . . ."));
+
+alert("String1ning type = " + typeof (string1) + "\nBollean1ning type =" + typeof (boolean1) + "\nNumber1ning type =" + typeof (number1));
